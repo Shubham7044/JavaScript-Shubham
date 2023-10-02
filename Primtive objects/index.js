9015ce8fa5b75bf2cd935c12d2b39a6edcbdd8e5
Shubham Swarnakar <s.swarnakar2003@gmail.com>
2023-10-02 20:48:55 +0530

    Add files via upload

---
+++ Primtive objects/index.js
@@ -0,0 +1,19 @@
+// nn bb ss u - Primitive in Js
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567") + BigInt("3")
+let e = "SHUBHAM"
+let f = Symbol("I am a nice symbol")
+let g = undefined
+console.log(a,b,c,d,e,g)
+console.log(typeof d)
+
+// Non Primitive Data Type - Objects in Js
+const item = {
+  "Shubham" : true,
+  "Arpa" : false,
+  "Lovish" : 54,
+  "Rohan" : undefined
+}
+console.log(item["Shubham"])
